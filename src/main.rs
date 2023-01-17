3a38b16a43b58534f31a37f7d8174cf4de6cb5eb
calexanderberg <carlalexanderberg@gmail.com>
2023-01-17 22:50:53 +0100

    Added the basics of framework with rust

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,18 @@
-fn main() {
-    println!("Hello, world!");
+use playwright::Playwright;
+
+#[tokio::main]
+async fn main() -> Result<(), playwright::Error> {
+    let playwright = Playwright::initialize().await?;
+    playwright.prepare()?; // Install browsers
+    let chromium = playwright.chromium();
+    let browser = chromium.launcher().headless(true).launch().await?;
+    let context = browser.context_builder().build().await?;
+    let page = context.new_page().await?;
+    page.goto_builder("https://example.com/").goto().await?;
+
+    // Exec in browser and Deserialize with serde
+    let s: String = page.eval("() => location.href").await?;
+    assert_eq!(s, "https://example.com/");
+    page.click_builder("a").click().await?;
+    Ok(())
 }
