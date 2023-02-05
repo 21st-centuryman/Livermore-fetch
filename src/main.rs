165ecaa45e2806d946a9c8abc2c1b69f8c78b48d
calexanderberg <carlalexanderberg@gmail.com>
2023-02-05 19:11:42 +0100

    Fx: removed the Error log and formated the code

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,9 @@
 use chrono::{DateTime, Datelike, TimeZone, Utc};
+use std::collections::HashSet;
 use tokio_test;
 use yahoo_finance_api as yahoo;
 
 fn main() {
-
     let now = Utc::now();
     let start = Utc
         .with_ymd_and_hms(now.year() - 10, now.month(), now.day(), 0, 0, 0)
@@ -12,24 +12,27 @@ fn main() {
         .with_ymd_and_hms(now.year(), now.month(), now.day(), 0, 0, 0)
         .unwrap();
 
-    let screener: Vec<String> = screener();
+    let screener: HashSet<String> = screener();
+
+    println!("{:?}", screener);
+    println!("{:?}", screener.len());
 
     for stock in screener {
         fetch_api(&stock, start, end);
     }
 }
 
-fn screener() -> Vec<String> {
+fn screener() -> HashSet<String> {
     let provider = yahoo::YahooConnector::new();
 
-    let mut screener: Vec<String> = Default::default();
+    let mut screener: HashSet<String> = Default::default();
 
     for a in 65u8..97 {
         // Goes from A to (right before a)
         let value = (a as char).to_string();
         let resp = tokio_test::block_on(provider.search_ticker(&value)).unwrap();
         for item in resp.quotes {
-            screener.push(item.symbol);
+            screener.insert(item.symbol);
         }
     }
 
@@ -39,14 +42,21 @@ fn screener() -> Vec<String> {
 fn fetch_api(stock: &str, start: DateTime<Utc>, end: DateTime<Utc>) {
     let provider = yahoo::YahooConnector::new();
     let resp = tokio_test::block_on(provider.get_quote_history(stock, start, end));
-    let _resp = match resp.unwrap().quotes() {
-        Ok(_resp) => println!("Success: {}", stock),
-        Err(msg) => println!("Stock. {}. Failed: {}", stock, msg),
+
+    match resp {
+        Ok(resp) => {
+            build_csv(stock, resp);
+        },
+        Err(_msg) => {
+            println!("Stock. {}. Failed.", stock);
+            println!("Moving to the next one");
+        }
     };
+}
 
 
-    // 
-    // COMMENT
-    // MAKE SURE THE FAILURE AT FETCHING DATA DOES NOT LEAD TO THE END OF THE EXECUTION
-    //
+fn build_csv(name: &str, resp: yahoo_finance_api::YResponse) {
+    for quote in resp.quotes().unwrap() {
+        println!("{} | {:?} | {:?}", name, quote.open, quote.close);
+    }
 }
